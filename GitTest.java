ddf705c0fb4a04058193fe4e4e320d5ac68dff47
Tom <t.litherland@lancaster.ac.uk>
2020-01-28 14:08:53 +0000

    hello

--- GitTest.java
+++ GitTest.java
@@ -2,5 +2,9 @@ public class GitTest {
 
     public static void main(String[] args) {
         System.out.println("Hello Git!");
+        for (int i = 0; i<10; i++){
+            System.out.println(i);
+        }
+        System.out.println(10);
     }
 }
